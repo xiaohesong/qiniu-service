b31788aaf048626bdbdb84521d4c8d7384ad172e
issue <didmehh@163.com>
2016-10-11 20:33:25 +0800

    fix

---
+++ lib/qiniu/service.rb
@@ -0,0 +1,10 @@
+require "qiniu/service/version"
+
+module Qiniu
+  module Service
+    # Your code goes here...
+    class << self
+      puts "Qiniu Service #{self}"
+    end
+  end
+end
---
+++ lib/qiniu/service/version.rb
@@ -0,0 +1,5 @@
+module Qiniu
+  module Service
+    VERSION = "0.1.0"
+  end
+end
